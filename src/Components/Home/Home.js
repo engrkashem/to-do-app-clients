b64829af1b2e6f6ba403523a4ad5420a7e52d227
engrkashem <kashemaust@gmail.com>
2022-05-19 03:26:34 +0600

    delete button working

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
+// import useTask from '../../hooks/useTask';
 import TaskCard from './TaskCard';
 
 const Home = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const onSubmit = data => {
-        const { email, password } = data;
 
+    const [toDoes, setToDoes] = useState([]);
+    const [success, setSuccess] = useState(false);
+    const [strikethroug, setStrikethroug] = useState('');
+
+    const url = `http://localhost:5000/task`;
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setToDoes(data))
+    }, [url, success]);
+
+    // const [success, setSuccess] = useState(false);
+    // const [toDoes] = useTask(success);
+
+    const onSubmit = data => {
+        // const { name, description } = data;
+        const url = `http://localhost:5000/task`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setSuccess(!success);
+                toast('Your To Do Lish is Updated.');
+            })
     };
+
+    const handleComplete = () => {
+        setStrikethroug('line-through');
+        toast.success('All Task Done. Congrats Me!!!!')
+    }
+
+    const deleteBtn = id => {
+        const proceed = window.confirm('Are You Sure !! Want to Delete the Task??');
+        if (proceed) {
+            const url = `http://localhost:5000/task/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setSuccess(!success);
+                })
+        }
+    }
+
     return (
         <div className="mockup-phone border-primary w-full h-full">
             <div className="camera w-full"></div>
@@ -16,7 +67,15 @@ const Home = () => {
                     <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row-reverse gap-20">
                             <div className="card  bg-base-100 shadow-xl grow">
-                                <TaskCard></TaskCard>
+                                {
+                                    toDoes.map(toDo => <TaskCard
+                                        key={toDo._id}
+                                        toDo={toDo}
+                                        strikethroug={strikethroug}
+                                        deleteBtn={deleteBtn}
+                                    ></TaskCard>)
+                                }
+
                             </div>
                             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                                 <div className="card-body">
@@ -64,6 +123,7 @@ const Home = () => {
                                         </div>
                                     </form>
                                 </div>
+                                <button onClick={handleComplete} className="btn btn-outline btn-success w-3/4 mx-auto block mb-5 font-bold">Complete</button>
                             </div>
                         </div>
                     </div>
--- src/Components/Home/TaskCard.js
+++ src/Components/Home/TaskCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const TaskCard = () => {
+const TaskCard = ({ toDo: { name, description, _id }, strikethroug, deleteBtn }) => {
+
+    const handleDelete = (id) => {
+        deleteBtn(id)
+    }
+
     return (
         <div className="card-body flex justify-between">
             <div className=' flex justify-center items-center gap-10'>
                 <div>
-                    <h2 className="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className={strikethroug ? `card-title text-2xl ${strikethroug}` : 'card-title text-2xl'}>{name}</h2>
+                    <p className={strikethroug ? `text-left mt-5 text-xl ${strikethroug}` : 'text-left mt-5 text-xl'}>{description}</p>
                 </div>
                 <div className="card-actions justify-end tooltip  tooltip-error" data-tip="Remove Task"   >
-                    <button className="btn btn-outline btn-error">X</button>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-outline btn-error">X</button>
                 </div>
             </div>
         </div>
