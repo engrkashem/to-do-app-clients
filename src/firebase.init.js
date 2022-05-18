88e463bb8f139b429a941703b6e03585f3dae220
engrkashem <kashemaust@gmail.com>
2022-05-18 22:51:58 +0600

    firebase.init.js and .enc.local is added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAwSZFnn-UJukJvBQQfkNXAeyB7IPtNxiY",
-    authDomain: "to-do-app-64383.firebaseapp.com",
-    projectId: "to-do-app-64383",
-    storageBucket: "to-do-app-64383.appspot.com",
-    messagingSenderId: "646332199636",
-    appId: "1:646332199636:web:5b60b43f6052e35e508141"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
