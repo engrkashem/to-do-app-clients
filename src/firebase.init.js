7398d2dcac8ebfc9d39c394859ba8393edf94704
engrkashem <kashemaust@gmail.com>
2022-05-18 22:02:08 +0600

    project initialization and all route setup, tailwind css and all other installation in client side done

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAwSZFnn-UJukJvBQQfkNXAeyB7IPtNxiY",
+    authDomain: "to-do-app-64383.firebaseapp.com",
+    projectId: "to-do-app-64383",
+    storageBucket: "to-do-app-64383.appspot.com",
+    messagingSenderId: "646332199636",
+    appId: "1:646332199636:web:5b60b43f6052e35e508141"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
