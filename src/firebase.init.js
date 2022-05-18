7e142ef3fbffe9636bab5c6c6faa9fb9b2b8ef52
engrkashem <kashemaust@gmail.com>
2022-05-19 03:31:58 +0600

    some modification

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    /* apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId, */
     apiKey: "AIzaSyAwSZFnn-UJukJvBQQfkNXAeyB7IPtNxiY",
     authDomain: "to-do-app-64383.firebaseapp.com",
     projectId: "to-do-app-64383",
